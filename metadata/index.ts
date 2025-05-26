320958772405e53ef9ed3524d780d2677da22c4e
ice-kele <1146907147@qq.com>
2025-05-27 00:43:33 +0800

    Update index.ts

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,7 +1,7 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
-export const siteDescription = '记录回忆，知识和畅想的地方'
+export const siteName = 'icekele的知识库'
+export const siteShortName = 'icekele'
+export const siteDescription = '记录学习历程、项目经验和技术成长的地方'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
